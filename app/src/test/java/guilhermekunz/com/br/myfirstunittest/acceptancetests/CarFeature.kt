78bf1ede51cda43e3b602dfabf202252147f702b
guilherme-kunz <guilhermebutzenkunz@gmail.com>
2022-05-06 10:24:01 -0300

    Second Failing Acceptance

--- app/src/test/java/guilhermekunz/com/br/myfirstunittest/acceptancetests/CarFeature.kt
+++ app/src/test/java/guilhermekunz/com/br/myfirstunittest/acceptancetests/CarFeature.kt
@@ -2,17 +2,26 @@ package guilhermekunz.com.br.myfirstunittest.acceptancetests
 
 import guilhermekunz.com.br.myfirstunittest.Car
 import junit.framework.Assert.assertEquals
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 
 class CarFeature {
 
-    private val car = Car(6.0)
+    private val engine = Engine()
+    private val car = Car(6.0, engine)
 
     @Test
     fun carIsLoosingFuelWhenItTurnsOn() {
         car.turnOn()
         assertEquals(5.5, car.fuel)
+    }
+
+    @Test
+    fun carIsTurningOnItsEngineAndIncreasesTheTemperature() {
+        car.turnOn()
+        assertEquals(95, car.engine.temperature)
+        assertTrue(car.engine.isTurnedOn)
     }
 
 }
